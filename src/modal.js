3816fd6ee2994d01b733466148eafbab61f70919
Sergey <pinchukov.net@gmail.com>
2020-08-10 00:24:45 +1000

    commin 2

--- src/modal.js
+++ src/modal.js
@@ -2,45 +2,34 @@ const contModal = document.querySelectorAll('.modal-container');
 const openModal = document.querySelectorAll('.btn-open');
 const displayBlock = 'modal-display-block';
 
-for(let i = 0; i < openModal.length; i++) {
-    openModal[i].addEventListener('click', open);
-};
-for(let i = 0; i < contModal.length; i++) {
-    contModal[i].addEventListener('click', close);
+for(let openModals of openModal) {
+    openModals.addEventListener('click', open);
 };
 
+for(let contModals of contModal) {
+    contModals.addEventListener('click', close);
+};
 
 function open(e) {
-    let dataId = e.target.dataset.id;
-    for(let i = 0; i < contModal.length; i++) {
-        if(contModal[i].classList.contains(dataId)){
-            contModal[i].classList.add(displayBlock);
-        };
+    for(let contModals of contModal) {
+       if(contModals.classList.contains(e.target.dataset.id)) contModals.classList.add(displayBlock);
     };
     param(e);
 };
 
 function close(e) {
     e.preventDefault();
-    if(e.target.classList.contains('btn-close')){
-        e.path[3].classList.remove(displayBlock);
-    }
-    if(e.target.classList.contains('modal-container')){
-        e.target.classList.remove(displayBlock);
-    }
+    if(e.target.classList.contains('btn-close')) e.path[3].classList.remove(displayBlock);
+    if(e.target.classList.contains('modal-container')) e.target.classList.remove(displayBlock);
 };
 
 function param(e) {
-    let dataWidth = e.target.dataset.width;
-    let dataEffect = e.target.dataset.effect;
-    let dataIndent = e.target.dataset.indent;
-    let dataBg = e.target.dataset.bg;
-    for(let i = 0; i < contModal.length; i++) {
-        if(contModal[i].classList.contains(displayBlock)){
-            contModal[i].children[0].style.width = dataWidth;
-            contModal[i].classList.add(dataEffect);
-            contModal[i].children[0].style.marginTop = dataIndent;
-            contModal[i].classList.add(dataBg);
+    for(let contModals of contModal) {
+        if(contModals.classList.contains(displayBlock)){
+            contModals.children[0].style.width = e.target.dataset.width;
+            contModals.classList.add(e.target.dataset.effect);
+            contModals.children[0].style.marginTop = e.target.dataset.indent;
+            contModals.classList.add(e.target.dataset.bg);
         };
     };
 };
